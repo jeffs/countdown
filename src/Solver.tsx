41c285d7c38f6e60cc55957e87540a58752cf877
Jeff Schwab <jeff@unbuggy.com>
2022-08-15 19:55:39 -0400

    Add radio buttons to choose tiles

--- src/Solver.tsx
+++ src/Solver.tsx
@@ -1,58 +1,96 @@
-import React from "react";
+import { ChangeEvent, MouseEvent, useState } from "react";
 
 import "./Solver.css";
 
-const TILE_COUNT = 6;
+const PLACE_COUNT: number = 6;
 
-function Dialog() {
-  return (
-    <dialog className="solver__dialog" open>
-      Hello, dialog.
-    </dialog>
-  );
-}
+const NUMBERS: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100];
 
-interface TileProps {
-  id: number;
+interface DialogProps {
+  onChange: (value: number) => void;
+  placeId: number;
   value?: number;
 }
 
-function Tile({ id, value }: TileProps) {
-  function handleClick(event: React.MouseEvent) {
-    alert(value);
+function Dialog(props: DialogProps) {
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    props.onChange(Number(event.target.value));
   }
 
   return (
-    <button
-      className="solver__tile"
-      onClick={handleClick}
-      title={`Tile #${id}`}
-    >
+    <div className="solver__dialog">
+      <h2>Tile #{props.placeId + 1}</h2>
+      {NUMBERS.map((value, index) => (
+        <label key={index}>
+          <input
+            checked={value === props.value}
+            name="number"
+            value={value}
+            onChange={handleChange}
+            type="radio"
+          />
+          {value}
+        </label>
+      ))}
+    </div>
+  );
+}
+
+interface PlaceProps {
+  id: number;
+  onClick?: () => void;
+  value?: number;
+}
+
+function Place({ id, onClick, value }: PlaceProps) {
+  return (
+    <button className="solver__place" onClick={onClick} title={`Place #${id}`}>
       {value}
     </button>
   );
 }
 
-function Row() {
-  const [values, setValues] = React.useState<Array<number | undefined>>(
-    Array(TILE_COUNT).fill(undefined)
-  );
+interface BoardProps {
+  onClick?: (placeId: number) => void;
+  values: Array<number | undefined>;
+}
 
+function Board({ onClick, values }: BoardProps) {
   return (
-    <div className="solver__row">
+    <div className="solver__board">
       {values.map((value, index) => (
-        <Tile id={index + 1} key={index} value={value} />
+        <Place
+          id={index + 1}
+          key={index}
+          onClick={() => onClick?.(index)}
+          value={value}
+        />
       ))}
     </div>
   );
 }
 
 export default function Solver() {
+  const [placeId, setPlaceId] = useState<number>(0);
+  const [values, setValues] = useState<Array<number | undefined>>(
+    Array(PLACE_COUNT).fill(undefined)
+  );
+
+  function handleChange(value: number) {
+    const newValues = [...values];
+    newValues[placeId] = value;
+    setValues(newValues);
+  }
+
   return (
     <>
       <h1>Solver</h1>
-      <Row />
-      <Dialog />
+      <Board onClick={setPlaceId} values={values} />
+      <Dialog
+        onChange={handleChange}
+        placeId={placeId}
+        value={values[placeId]}
+      />
     </>
   );
 }
