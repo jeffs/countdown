a29ccd92e430b5ba89741a08f248f0fe7e609eb4
Jeff Schwab <jeff@unbuggy.com>
2022-08-19 17:37:20 -0400

    WIP on Rust solver

--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -1,4 +1,28 @@
+use std::collections::{HashMap, VecDeque};
+use std::fmt;
 use wasm_bindgen::prelude::*;
+
+#[allow(dead_code)]
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+enum Expr {
+    Val(i32),
+    Add(usize, usize),
+    Sub(usize, usize),
+    Mul(usize, usize),
+    Div(usize, usize),
+}
+
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Expr::Val(val) => write!(f, "{val}"),
+            Expr::Add(left, right) => write!(f, "({left}) + ({right})"),
+            Expr::Sub(left, right) => write!(f, "({left}) - ({right})"),
+            Expr::Mul(left, right) => write!(f, "({left}) * ({right})"),
+            Expr::Div(left, right) => write!(f, "({left}) / ({right})"),
+        }
+    }
+}
 
 // The initial values form the root node of a tree, in which each node is a
 // multiset of expressions.  We do a Breadth First Search (BFS) of this tree,
@@ -22,10 +46,45 @@ use wasm_bindgen::prelude::*;
 // The number of values in Countdown is always 6, but I am reliably informed
 // that the trait `FromWasmAbi` is not implemented for `[i32; 6]`.
 #[wasm_bindgen]
-pub fn solve(target: i32, values: &[i32]) -> String {
+pub fn solve(_target: i32, values: &[i32]) -> String {
     assert_eq!(values.len(), 6);
 
-    format!("TODO: solve({target}, {values:?})")
+    // A vec of all unique expressions, and a map from expressions to their
+    // indexes in the vec.
+    let mut expr_vec: Vec<Expr> = Vec::new();
+    let mut expr_map: HashMap<Expr, usize> = HashMap::new();
+
+    for &val in values {
+        let val = Expr::Val(val);
+        if !expr_map.contains_key(&val) {
+            expr_map.insert(val, expr_vec.len());
+            expr_vec.push(val);
+        }
+    }
+
+    // Queue of multisets.  Each multiset is a vec of indexes into expr_vec.
+    let mut queue: VecDeque<Vec<usize>> = VecDeque::new();
+    queue.push_back(values.iter().map(|&v| expr_map[&Expr::Val(v)]).collect());
+
+    if let Some(expr_indexes) = queue.pop_front() {
+        let strings: Vec<_> = expr_indexes
+            .into_iter()
+            .map(|i| expr_vec[i])
+            .map(|x| x.to_string())
+            .collect();
+        strings.join("/")
+
+    //     for i in 0..exprs.len() {
+    //         for j in (i + 1)..exprs.len() {
+    //             let mut new_exprs = Vec::new();
+    //             new_exprs.append(&exprs[0..i]);
+    //         }
+    //     }
+    } else {
+        "oops".to_string()
+    }
+
+    //format!("TODO: solve({target}, {values:?})")
 }
 
 #[cfg(test)]
@@ -34,6 +93,7 @@ mod tests {
 
     #[test]
     fn test_solve_trivial() {
-        assert_eq!(solve(100, &[100; 6]), "100");
+        solve(100, &[100; 6]);
+        //assert_eq!(solve(100, &[100; 6]), "100");
     }
 }
