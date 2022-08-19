a642c8a032bf9b63887b183aef0dd68ece23fe5b
Jeff Schwab <jeff@unbuggy.com>
2022-08-19 17:53:04 -0400

    Fix build-wasm to await the child process

--- scripts/build-wasm.mjs
+++ scripts/build-wasm.mjs
@@ -7,22 +7,22 @@ import { copyFile } from "node:fs/promises";
 const exec = util.promisify(child_process.exec);
 
 async function build() {
-  const { child } = exec("wasm-pack build --target no-modules", {
+  const promise = exec("wasm-pack build --target no-modules", {
     cwd: "wasm-lib",
   });
-  child.stdout.on("data", (data) => {
+  promise.child.stdout.on("data", (data) => {
     process.stdout.write(data);
   });
-  child.stderr.on("data", (data) => {
+  promise.child.stderr.on("data", (data) => {
     process.stderr.write(data);
   });
-  child.on("close", (status) => {
+  promise.child.on("close", (status) => {
     if (status !== 0) {
       console.error(`error: child exited with status ${status}`);
       process.exit(status);
     }
   });
-  await child;
+  await promise;
 }
 
 async function main() {
